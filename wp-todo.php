b6ab8508a7dd6c21596417421c109b3a7fdfb4c0
Ng Qi Teng <Cmdcool3@gmail.com>
2022-07-17 15:27:58 +0800

    Initial Folder Structure

---
+++ wp-todo.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package wp-todo
+ */
+/**
+ * Plugin Name: WordPress Todo
+ * Plugin URI: 
+ * Description: A todo list in WordPress
+ * Version: 1.0.0
+ * Author: Ng Qi Teng
+ * Author URL:
+ * License:
+ * License URL:
+ * Text Domain:  wp-todo
+ */
+
+( ! defined( 'ABSPATH' ) ) ? exit() : null;
+
+( ! class_exists( '' ) ) ? null : null;
+
+define( 'PLUGIN_DIR_PATH'. plugin_dir_path( __FILE__ ) );
+define( 'PLUGIN_DIR_URL'. plugin_dir_url( __FILE__ ) );
