57d96c6d21400fcbd6b1e9e079e3169e1ac8a6c2
Ng Qi Teng <Cmdcool3@gmail.com>
2022-07-24 02:34:14 +0800

    Update Settings API
    add admin pages
    add admin sub pages

--- inc/Api/Settings_Api.php
+++ inc/Api/Settings_Api.php
@@ -9,12 +9,13 @@ class Settings_Api
 {
 
     public $admin_pages = array();
+    public $admin_sub_pages = array();
 
     public function register()
     {
         if ( !empty( $this->admin_pages ) ) 
         {
-            add_action( 'admin_menu'. array( $this, 'add_admin_menu' ) );
+            add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
         }
     }
 
@@ -24,11 +25,46 @@ class Settings_Api
         return $this;
     }
 
+    public function add_sub_pages( array $sub_pages )
+    {
+        $this->admin_sub_pages = array_merge( $this->admin_sub_pages, $sub_pages );
+        return $this;
+    }
+
+    public function with_sub_page( string $title = null )
+    {
+        if ( empty( $this->admin_pages ) )
+        {
+            return $this;
+        }
+
+        $admin_page = $this->admin_pages[ 0 ];
+
+        $sub_pages = array(
+            array(
+                'parent_slug' => $admin_page[ 'menu_slug' ],
+                'page_title'  => $admin_page[ 'page_title' ],
+                'menu_title'  => ( $title ) ? $title : $admin_page[ 'menu_title' ],
+                'capability'  => $admin_page[ 'capability' ],
+                'menu_slug'   => $admin_page[ 'menu_slug' ],
+                'callback'    => function() { echo 'sub page'; },
+            ),
+        );
+
+        $this->admin_sub_pages = $sub_pages;
+        
+        return $this;
+    }
+
     public function add_admin_menu()
     {
         foreach ( $this->admin_pages as $page )
         {
             add_menu_page( $page[ 'page_title' ], $page[ 'menu_title' ], $page[ 'capability' ], $page[ 'menu_slug' ], $page[ 'callback' ], $page[ 'icon_url' ], $page[ 'position' ] );
+        }
+        foreach ( $this->admin_sub_pages as $sub_page )
+        {
+            add_submenu_page( $sub_page[ 'parent_slug' ], $sub_page[ 'page_title' ], $sub_page[ 'menu_title' ], $sub_page[ 'capability' ], $sub_page[ 'menu_slug' ], $sub_page[ 'callback' ] );
         }
     }
 }
--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -6,18 +6,59 @@
 namespace Inc\Pages;
 
 use \Inc\Base\Base_Controller;
+use \Inc\Api\Settings_Api;
 
 class Admin extends Base_Controller
 {
+	public $settings;
+	public $pages = array();
+	public $sub_pages = array();
+
+	public function __construct()
+	{
+		$this->settings = new Settings_Api();
+
+		$this->pages = array(
+			array(
+				'page_title' => 'WP Todo Plugin',
+				'menu_title' => 'WP Todo',
+				'capability' => 'manage_options',
+				'menu_slug'  => 'wp_todo_plugin',
+				'callback'   => function() {  },
+				'icon_url'   => 'dashicons-store',
+				'position'   => '110',
+			),
+		);
+
+		$this->sub_pages = array(
+			array(
+				'parent_slug' => 'wp_todo_plugin',
+				'page_title' => 'Sub 01',
+				'menu_title' => 'Sub 01',
+				'capability' => 'manage_options',
+				'menu_slug'  => 'sub_01',
+				'callback'   => function() {  },
+			),
+			array(
+				'parent_slug' => 'wp_todo_plugin',
+				'page_title' => 'Sub 02',
+				'menu_title' => 'Sub 02',
+				'capability' => 'manage_options',
+				'menu_slug'  => 'sub_02',
+				'callback'   => function() {  },
+			),
+			array(
+				'parent_slug' => 'wp_todo_plugin',
+				'page_title' => 'Sub 03',
+				'menu_title' => 'Sub 03',
+				'capability' => 'manage_options',
+				'menu_slug'  => 'sub_03',
+				'callback'   => function() {  },
+			),
+		);
+	}
+
 	public function register() {
-		add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
-	}
-
-	public function add_admin_pages() {
-		add_menu_page( 'WP Todo Plugin', 'WP Todo', 'manage_options', 'wp_todo_plugin', array( $this, 'admin_index' ), 'dashicons-store', 110 );
-	}
-
-	public function admin_index() {
-		require_once $this->plugin_path . 'templates/admin.php';
+		$this->settings->add_pages( $this->pages )->with_sub_page( 'General' )->add_sub_pages( $this->sub_pages )->register();
 	}
 }
